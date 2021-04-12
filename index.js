239f6809635c4567a7693fe0ac716cc836ba7f98
Jairo <jjairojr00@gmail.com>
2021-04-12 17:23:07 -0300

    :rocket: add typeip

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 
 var prompt = require('prompt-sync')();
-
+let ipAddress = "";
 async function getValue(a, b, operator) {
-  const response = await axios.post('http://localhost:3333/', {a, b, operator});
+  const response = await axios.post(`http://${ipAddress}:3333`, {a, b, operator});
   console.log(response.data);
 }
 
 function start() {
+  ipAddress = prompt('Primeiramente indique o IP do servidor? ');
   const a = prompt('Primeiro valor? ');
   const b = prompt('Segundo valor? ');
   const operator = prompt('Operador (+, -, * ou /)? ');
