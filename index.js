5619f1e3368fcff780d99f1e6c420d0aeeeb72de
Jairo <jjairojr00@gmail.com>
2021-04-13 20:58:01 -0300

    :rocket: add ip

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 var prompt = require('prompt-sync')();
 let ipAddress = "";
 async function getValue(a, b, operator) {
-  const response = await axios.post(`http://${ipAddress}:3333`, {a, b, operator});
+  const response = await axios.post(`http://172.31.41.9:3333`, {a, b, operator});
   console.log(response.data);
 }
 
