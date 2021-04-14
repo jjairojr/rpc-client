a2c6852d38e7d1b31bbe351d5ac3779db8a98e6e
Jairo <jjairojr00@gmail.com>
2021-04-13 21:04:26 -0300

    :rocket: remove dinamic ip

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const axios = require('axios');
 
 var prompt = require('prompt-sync')();
-let ipAddress = "";
 async function getValue(a, b, operator) {
   const response = await axios.post(`http://172.31.41.9:3333`, {a, b, operator});
   console.log(response.data);
 }
 
 function start() {
-  ipAddress = prompt('Primeiramente indique o IP do servidor? ');
   const a = prompt('Primeiro valor? ');
   const b = prompt('Segundo valor? ');
   const operator = prompt('Operador (+, -, * ou /)? ');
